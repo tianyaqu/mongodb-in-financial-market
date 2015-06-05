178868ba58baa6a237c2aee12ea0dfab46a6ccbd
tianyaqu <tianyaqu@qq.com>
2015-06-05 22:32:06 +0800

    could retrive ticks data

--- query/R/query.R
+++ query/R/query.R
@@ -1,9 +1,11 @@
+library(decompress)
+library(xts)
 GetBar <- function(market, contract_id, type, start, end = as.POSIXlt(Sys.time(), "GMT"), limit = 1000) {
   ip <- '127.0.0.1'
   port <- '27017'
   uri <- paste(ip,port,sep=':')
   mongo <- rmongodb::mongo.create(uri)
-  fields <- c('Timestamp','InstrumentID','Open','High','Low','Close','Volume','Turnover')
+  fields <- c('Timestamp','Open','High','Low','Close','Volume','Turnover')
   
   if(rmongodb::mongo.is.connected(mongo) == TRUE) {
     coll = paste(market,type,sep='.') 
@@ -13,28 +15,53 @@ GetBar <- function(market, contract_id, type, start, end = as.POSIXlt(Sys.time()
     records <- rmongodb::mongo.find.all(mongo, coll, query = list('InstrumentID'= contract_id,'Timestamp' = list('$gte' = start), 'Timestamp' = list('$lt' = end)),limit = limit)
     df <- do.call("rbind", lapply(records, data.frame))
   }
-  
-  return (df[fields])
+  data <- df[fields]
+  df_xts <- xts(data[,-1],order.by=as.POSIXct(df$Timestamp))
+  return (df_xts)
 }
 
 GetTicks <- function(market, contract_id,fields,start, end = as.POSIXlt(Sys.time(), "GMT"), limit = 30) {
-  ip <- '127.0.0.1'
-  port <- '27017'
-  uri <- paste(ip,port,sep=':')
-  mongo <- rmongodb::mongo.create(uri)
-  
-  if(rmongodb::mongo.is.connected(mongo) == TRUE) {
+    ip <- '127.0.0.1'
+    port <- '27017'
+    uri <- paste(ip,port,sep=':')
+    mongo <- rmongodb::mongo.create(uri)
+
+    if(rmongodb::mongo.is.connected(mongo) == TRUE) {
     coll = paste(market,'ticks',sep='.')  
-    
+
     records <- rmongodb::mongo.find.all(mongo, coll, query = list('InstrumentID'= contract_id,'Timestamp' = list('$gte' = start), 'Timestamp' = list('$lt' = end)),limit = limit)
-    #df <- do.call("rbind", lapply(records, data.frame))
+    len <- length(records)
+    
     for (i in 1:length(records))
     {
-      t <- records[[i]]$Timestamp
-      paste("total number is",str(t))
-    }
+        t <- records[[i]]$Timestamp
+        paste("total number is",str(t))
+        raw <- records[[i]]$Data
+        df = R_decompress(market,raw)
+        year <- df[,'day']%/%10000
+        t_tmp <- df[,'day']%%10000
+        month <- t_tmp%/%100
+        day <- t_tmp%%100
 
+        hour <- df[,'updatetime']%/%10000
+        t_tmp1 <- df[,'updatetime']%%10000
+        minute <- t_tmp1%/%100
+        sec <- t_tmp1%%100
+        millisec <- df[,'millsec']/1000
+
+        ts <- ISOdatetime(year,month,day,hour,minute,sec,tz="GMT")+millisec
+        df$timestamp <- ts
+        
+        #delete character columns
+        df$id <- NULL
+        
+
+        if(length(fields) <= 0)
+            return (df)
+        else
+            data <- df[c('timestamp',fields)]
+            df_xts <- xts(data[,-1],order.by=as.POSIXct(df$timestamp))     
+    }
+    return (df_xts)
   }
-  
-  return (records)
-}  
+}
